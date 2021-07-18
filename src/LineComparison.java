e897bf66eca43c06523246eb6252cdefbe15114f
gosavisomnath <somnathgosavi29.sg@gmail.com>
2021-07-18 10:06:27 +0530

    Added Find Length Code

--- src/LineComparison.java
+++ src/LineComparison.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 public class LineComparison {
+
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-        System.out.println("Welcome to Line Comparison Problem");
+        System.out.println("Enter X1,Y1,X2,Y2:");
+        Scanner sc= new Scanner(System.in);
+        int X1=sc.nextInt();
+        int Y1=sc.nextInt();
+        int X2=sc.nextInt();
+        int Y2=sc.nextInt();
+        int lenth=((X2-X1)*2+(Y2-Y1)*2);
+        System.out.println(lenth);
+
     }
+
 }
